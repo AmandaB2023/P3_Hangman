88889ccdc455d7ef9872edea2f2dd30ec8d3102e
Amanda Fahy <amandafahy@ymail.com>
2024-03-04 21:42:04 +0000

    Add clear function and user name input.

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import os
+
+
+"""
+Clear function to clean-up the terminal.
+"""
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+def get_user_name():
+    name = ''
+    while True:
+        name = input ("What is your name ?\n").capitalize()
+        if name.isalpha() == True:
+            print('')
+            clear_screen()
+            print(f""" Hello, " + name """)
+            break
+        else:
+            print(f""" Please enter your name using letters only. """)
+    return name
