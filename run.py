cb2673671e3cf329759279dbb5aea53a46ab7a4e
Amanda Fahy <amandafahy@ymail.com>
2024-03-05 15:58:52 +0000

    Remove welcome.py file.

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import random
 import string
 import os
 import time
+from art import *
 from time import sleep
 from colorama import Fore, Style
 from words import words
@@ -9,11 +10,12 @@ from hangman_visual import hangman_visual_dict
 from welcome import logo
 
 
-"""
+"""S
 Prints logo
 """
 def hello():
-    print(logo)
+    Art = text2art("HANGMAN")
+    print(Art)
 
 
 """
@@ -26,14 +28,15 @@ def clear_screen():
 def get_user_name():
     name = ''
     while True:
-        name = input (f"""{Fore.GREEN }"What is your name ?\n{Style.RESET_ALL}""").capitalize()
+        name = input(f"""{Fore.GREEN }"What is your name ?\n
+        {Style.RESET_ALL}""").capitalize()
         if name.isalpha() == True:
             print('')
             clear_screen()
             print("Hello, " + name)
             break
-        else:     
-            print(f"""{Fore.RED}Please enter your name using letters only!{Style.RESET_ALL}""")
+        else:
+             print(f"""{Fore.RED}Please enter your name using letters only!{Style.RESET_ALL}""")
     return name
 
 
@@ -44,45 +47,45 @@ def restart():
     if restart.upper() == 'Y':
         clear_screen()
         time.sleep(1)
-        print (f"""{Fore.GREEN} Restarting...\n {Style.RESET_ALL}""")
+        print(f"""{Fore.GREEN} Restarting...\n {Style.RESET_ALL}""")
         time.sleep(0.5)
         clear_screen()
         start_game()
-    elif restart.upper() =='N':
+    elif restart.upper() == 'N':
         clear_screen()
         print(f"""{Fore.GREEN}Goodbye{Style.RESET_ALL}""")
         exit()
 
+
 """
 Randomly choose word from list.
-"""        
+"""
+
+
 def get_valid_word(words):
-    word = random.choice(words) 
+    word = random.choice(words)
     while '-' in word or ' ' in word:
         word = random.choice(words)
     return word.upper()
 
+
 """
-Start game , set number of letters in word 
+Start game,set number of letters in word
 and check what letters the user has guessed.
-""" 
+"""
 def start_game():
     word = get_valid_word(words)
-    word_letters = set(word)  
+    word_letters = set(word)
     alphabet = set(string.ascii_uppercase)
-    used_letters = set()  
+    used_letters = set()
 
     lives = 7
 
-    """ 
-    Get the users letter input.
-    """
-    while len(word_letters) > 0 and lives > 0 : 
+    # Get the users letter input.
+    while len(word_letters) > 0 and lives > 0 :
         # letters used
-        print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
-        """
-        The  current word is (ie. W - R D)
-        """
+        print('You have' +Fore.GREEN, lives, 'lives left and you have used these letters: ', '  '.join(used_letters))
+        #The  current word is (ie. W - R D)
         word_list = [letter if letter in used_letters else '-' for letter in word]
         print(hangman_visual_dict[lives])
         print('Your current word is : ', ' '.join(word_list))
@@ -97,13 +100,15 @@ def start_game():
                 """
                 Takes away a life if letter is not in the word.
                 """
-                lives = lives - 1  
+                lives = lives - 1
                 print('Sorry but your chosen letter,', user_letter, 'is not in the word.\n')
-               
+
         elif user_letter in used_letters:
-            print(f"""\n Oops you have already used that letter. Have another guess.""")
+            print(f"""\n Oops you have already used that letter.
+            Have another guess.""")
         else:
-            print(f""" \n {Fore.RED} Sorry but that is not a valid letter.{Style.RESET_ALL}""")
+            print(f""" \n {Fore.RED} Sorry but that is not a valid letter.
+            {Style.RESET_ALL}""")
 
     """
     Game ends when letter == 0 OR when lives == 0 OR user guesses the correct word.
@@ -117,6 +122,8 @@ def start_game():
         clear_screen()
         print('YAY! You guessed the word', word, '!!')
         print('')
+        tprint ("CONGRADULATIONS")
+        print()
         print(f"""{Fore.CYAN} CONGRATULATIONS!!!{Style.RESET_ALL}""")
         time.sleep(2)
         clear_screen()
@@ -127,25 +134,24 @@ game_options = {
 
     1: 'Play game ',
     2: 'Rules',
-    3: 'Exit' ,
-    
+    3: 'Exit',
 }
+
 
 def print_menu(user_name):
     print(f"""{Fore.GREEN}Please select one of the following options by typing the cooresponding number and hitting Enter.{Style.RESET_ALL}""")
     for key in game_options.keys():
-        print (key, '--', game_options[key] )
-    while(True):
+        print(key,'--', game_options[key])
+    while (True):
         option = ''
         try:
-            option = int(input('Please enter your selected menu option here :    '))
+            option = int(input('Please enter your selected menu option here  :  '))
         except:
             print(f"""{Fore.RED}Incorrect input type.{Style.RESET_ALL}""")
 
-
         """
         Check what option was entered by the user.
-        """ 
+        """
         if option == 1:
             option1()
             start_game()
@@ -159,15 +165,15 @@ def print_menu(user_name):
             print(f"""{Fore.GREEN}Checking what option you have choosen...{Style.RESET_ALL}""")
             sleep(2)
             clear_screen()
-            print(f"""{Fore.RED}Sorry that option was invalid. Please enter a number between 1 and 3.{Style.RESET_ALL}""")
+            print(f"""{Fore.RED}Sorry that option was invalid.
+            Please enter a number between 1 and 3.{Style.RESET_ALL}""")
             print_menu(user_name)
-            
 
 
 def option1():
     clear_screen()
     time.sleep(1)
-    print (f"""{Fore.GREEN}Lets Play.....\n {Style.RESET_ALL}""")
+    print(f"""{Fore.GREEN}Lets Play.....\n {Style.RESET_ALL}""")
     time.sleep(0.5)
 
 
@@ -179,7 +185,7 @@ def option2(user_name):
 def option3(user_name):
     clear_screen()
     print(f"""{Fore.MAGENTA}Thanks for playing !!{Style.RESET_ALL}""")
-    print("Goodbye, " +    user_name )
+    print("Goodbye, " + user_name)
 
 
 def rules(user_name):
@@ -195,15 +201,16 @@ def rules(user_name):
     * If you lose all 7 lives, the game will end.
     * You cannot exit the game once it begins.
         {Style.RESET_ALL} """)
-    input(f"""Hit enter to go back to the menu. \n""") 
-    clear_screen() 
+    input(f"""Hit enter to go back to the menu. \n""")
+    clear_screen()
     sleep(1)
     print_menu(user_name)
+
 
 def main():
     hello()
     user_name = get_user_name()
-    print_menu(user_name)    
+    print_menu(user_name)
 
 
 if __name__=='__main__':
