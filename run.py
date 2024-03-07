a7bf59e5954fddb59ccdec60407f27b35085b34d
Amanda Fahy <amandafahy@ymail.com>
2024-03-07 02:18:15 +0000

    Update code after validation

--- run.py
+++ run.py
@@ -8,19 +8,24 @@ from words import words
 from hangman_visual import hangman_visual_dict
 
 
-
 """ Prints logo """
+
+
 def hello():
     Art = text2art("  HANGMAN  ")
     print(Art)
 
 
 """ Clear function to clean-up the terminal."""
+
+
 def clear_screen():
     os.system("cls" if os.name == "nt" else "clear")
 
 
 """ Ask for users name """
+
+
 def get_user_name():
     print(f"""Welcome to Hangman""")
     name = ''
@@ -28,17 +33,20 @@ def get_user_name():
         name = input(f"""{Fore.GREEN } What is your name ?\n
         {Style.RESET_ALL}""").capitalize()
         print(' Please enter at least 3 letters.')
-        if name.isalpha() and len (name) > 2:
+        if name.isalpha() and len(name) > 2:
             print('')
             clear_screen()
             print("Hello, " + name)
             break
         else:
-            print(f"""{Fore.RED}Please enter your name using letters only! and press Enter{Style.RESET_ALL}""")
+            print(f"""{Fore.RED}Please enter your name using letters only!
+            and press Enter{Style.RESET_ALL}""")
     return name
 
 
 """Randomly choose word from list."""
+
+
 def get_valid_word(words):
     word = random.choice(words)
     while '-' in word or ' ' in word:
@@ -46,8 +54,10 @@ def get_valid_word(words):
     return word.upper()
 
 
-""" Start game,set number of letters in word and check 
+""" Start game,set number of letters in word and check
 what letters the user has guessed."""
+
+
 def start_game():
     word = get_valid_word(words)
     word_letters = set(word)
@@ -57,10 +67,11 @@ def start_game():
     lives = 7
 
     # Get the users letter input.
-    while len(word_letters) > 0 and lives > 0 :
+    while len(word_letters) > 0 and lives > 0:
         # letters used
-        print('You have',lives, 'lives left and you have used these letters: ', '  '.join(used_letters))
-        #The  current word is (ie. W - R D)
+        print(f"""You have', lives, 'lives left
+        and you have used these letters: ',' '.join(used_letters)""")
+        # The  current word is (ie. W - R D)
         word_list = [letter if letter in used_letters else '-' for letter in word]
         print(hangman_visual_dict[lives])
         print('Your current word is : ', ' '.join(word_list))
@@ -73,9 +84,11 @@ def start_game():
                 word_letters.remove(user_letter)
                 print('')
             else:
-            # Takes away a life if letter is not in the word.
+                # Takes away a life if letter is not in the word.
                 lives = lives - 1
-                print('Sorry but your chosen letter,', user_letter, 'is not in the word.\n')
+                print(f"""
+                Sorry but your chosen letter,', user_letter,
+                'is not in the word.\n""")
 
         elif user_letter in used_letters:
             print(f"""\n Oops you have already used that letter.
@@ -84,7 +97,7 @@ def start_game():
             print(f""" \n {Fore.RED} Sorry but that is not a valid letter.
             {Style.RESET_ALL}""")
 
-    # Game ends when letter == 0 OR when lives == 0 OR user guesses the correct word.
+    # Game ends if letter == 0 OR lives == 0 OR user gets correct word.
     if lives == 0:
         print(hangman_visual_dict[lives])
         print('Uh Oh you died, sorry. The word was', word)
@@ -109,18 +122,20 @@ game_options = {
 
 
 """ Prints menu options and asks user to selet an option"""
+
+
 def print_menu(user_name):
-    print(f"""{Fore.GREEN}Please select one of the following options by typing 
+    print(f"""{Fore.GREEN}Please select one of the following options by typing
     the cooresponding number and hitting Enter.{Style.RESET_ALL}""")
     for key in game_options.keys():
         print(key, '--', game_options[key])
     while (True):
         option = ''
         try:
-            option = int(input('Please enter your selected menu option here  :  '))
-        except:
-            ValueError
-            print(f"""{Fore.RED}Incorrect input type.{Style.RESET_ALL}""")
+            option = int(input(
+                'Please enter your selected menu option here  :'))
+        except ValueError as e:
+            print(f"""{Fore.RED}Incorrect input type.:{e}{Style.RESET_ALL}""")
         # Check what option was entered by the user.
         if option == 1:
             option1()
@@ -132,7 +147,8 @@ def print_menu(user_name):
             option3(user_name)
             exit()
         else:
-            print(f"""{Fore.GREEN}Checking what option you have choosen...{Style.RESET_ALL}""")
+            print(f"""{Fore.GREEN}
+            Checking what option you have choosen...{Style.RESET_ALL}""")
             sleep(2)
             clear_screen()
             print(f"""{Fore.RED}Sorry that option was invalid.
@@ -159,6 +175,8 @@ def option3(user_name):
 
 
 """ Game rules and how to play."""
+
+
 def rules(user_name):
     clear_screen
     print(f"""{Fore.BLUE}
@@ -176,11 +194,13 @@ def rules(user_name):
 
 
 """Restart function to restart the game."""
+
+
 def restart():
     clear_screen()
     print(f"""{Fore.GREEN} Would you like to play again ? {Style.RESET_ALL}""")
     while True:
-        restart = input(""" For Yes please press Y and hit Enter , 
+        restart = input(""" For Yes please press Y and hit Enter ,
                         For No just hit Enter\n""")
         if restart.upper() == 'Y':
             clear_screen()
@@ -191,7 +211,9 @@ def restart():
             start_game()
         else:
             clear_screen()
-            print(f""" {Fore.BLUE} Goodbye, Thanks for playing , we hope you had fun!!!{Style.RESET_ALL}""")
+            print(f""" {Fore.BLUE}
+            Goodbye, Thanks for playing ,
+            we hope you had fun!!!{Style.RESET_ALL}""")
             exit()
 
 
@@ -201,6 +223,5 @@ def main():
     print_menu(user_name)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
-
